274d9b8b72b097c578ebe3686f2cc426af0a920b
acm-cont <acm-cont>
2000-08-18 12:08:01 +0000

    update

--- judge/lib/judging_common.tcl
+++ judge/lib/judging_common.tcl
@@ -11,9 +11,9 @@
 # RCS Info (may not be true date or author):
 #
 #   $Author: acm-cont $
-#   $Date: 2000/08/18 11:26:08 $
+#   $Date: 2000/08/18 12:08:02 $
 #   $RCSfile: judging_common.tcl,v $
-#   $Revision: 1.1 $
+#   $Revision: 1.2 $
 #
 
 # Include this code in TCL program via:
@@ -26,17 +26,15 @@
 #
 set judging_parameters_file "hpcm_judging.rc"
 
-# List of directories to search for judging parameters
-# file:
+# Function that prints a fatal error on the standard
+# output and exits the program with an error code.  Each
+# argument is printed on a separate line.
 #
-set judging_parameters_directories \
-    ". .. ../.. ../../.. ../../../.."
-
 proc fatal_error { args } {
-    puts "ERROR: [lindex $args 0]"
+    puts "FATAL ERROR: [lindex $args 0]"
     set args [lreplace $args 0 0]
     foreach m $args {
-    	puts "       $m"
+    	puts "             $m"
     }
     exit 1
 }
@@ -47,31 +45,39 @@ if { [info command signal] == "signal" } {
     signal error SIGINT
 }
 
-# Locate the judging parameters file name.  This
-# will be returned as value of the initialize_
-# program function.
+# Locate the directory containing the judging
+# parameters file.  This should be unique.
 #
-set judging_parameters_list ""
-foreach d $judging_parameters_directories {
+set judging_parameters_file_list ""
+foreach d ". .. ../.. ../../.. ../../../.." {
     if { [file exists \
 	       "$d/$judging_parameters_file"] } {
 	lappend judging_parameters_list \
 		"$d/$judging_parameters_file"
+	set judging_parameters_directory $d
     }
 }
 
-if { [llength $judging_parameters_list] == 1 } {
-    if { [file readable $judging_parameters_list] } {
-	source $judging_parameters_list
+# If directory containing judging parameters file
+# is unique, source the file, and set juding_parameters_
+# directory to the directory name.  Otherwise call
+# fatal_error.
+#
+if { [llength $judging_parameters_file_list] == 1 } {
+    if { [file readable \
+               $judging_parameters_file_list] } {
+	source $judging_parameters_file_list
     } else {
 	fatal_error \
-	    "$judging_parameters_list not readable"
+	    "$judging_parameters_file_list\
+	     not readable"
     }
-} elseif { [llength $judging_parameters_list] == 0 } {
+} elseif { [llength $judging_parameters_file_list] \
+           == 0 } {
     fatal_error \
 	"$judging_parameters_file not found"
 } else {
     fatal_error \
 	"Too many $judging_parameters_file files:" \
-	$judging_parameters_list
+	$judging_parameters_file_list
 }
