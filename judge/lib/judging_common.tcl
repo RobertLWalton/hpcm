7f7ce69b003d3f2d487364a88f50b2a53f0bd3bd
acm-cont <acm-cont>
2000-08-20 14:26:49 +0000

    update

--- judge/lib/judging_common.tcl
+++ judge/lib/judging_common.tcl
@@ -11,9 +11,9 @@
 # RCS Info (may not be true date or author):
 #
 #   $Author: acm-cont $
-#   $Date: 2000/08/19 15:56:06 $
+#   $Date: 2000/08/20 14:26:51 $
 #   $RCSfile: judging_common.tcl,v $
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
 
 # Include this code in TCL program via:
@@ -21,24 +21,86 @@
 #	set lib_directory \
 #	    "[file dirname $argv0]/../lib"
 #	source "$lib_directory/judging_common.tcl"
+#	catch {
+#
+#	... your program ...
+#	.., (do not change argc, argv0, argv)
+#	... terminates with `exit 0' ...
+#
+#	} caught_output
+#	caught_error
+#
+
+# The catch and `caught_error' function catches all
+# program errors and causes them to be announced on the
+# standard output, be logged in the error log file, and
+# return an exit code of 1 from the program.
+
+# Default error log file name.  For use if we cannot
+# find judging parameters.
+#
+set default_error_log_file "~/HPCM_Error_Log"
 
 # Judging parameters file name:
 #
 set judging_parameters_file "hpcm_judging.rc"
 
-# Function that prints a fatal error on the standard
-# output and exits the program with an error code.  Each
-# argument is printed on a separate line.
+# Function called at end of program when a fatal error
+# in the program is caught.
 #
-proc fatal_error { args } {
-    puts "FATAL ERROR: [lindex $args 0]"
-    set args [lreplace $args 0 0]
-    foreach m $args {
-    	puts "             $m"
-    }
+proc caught_error {} {
+    global caught_output
+    log_error $caught_output
     exit 1
 }
 
+# Function called to log an error when the program
+# may want to continue.
+#
+proc log_error { error_output } {
+
+    global argv0 argv errorCode errorInfo \
+	   default_error_log_file \
+	   log_directory error_log_file
+
+    puts "ERROR caught for $argv0 $argv"
+    puts $error_output
+
+    if { [info exists log_directory] \
+         && [info exists error_log_file] \
+	 && $log_directory != "" \
+	 && $error_log_file != "" } {
+
+        set log_file "$log_directory/$error_log_file"
+
+	if { ! [file writable $log_file] } {
+	    if { [catch {
+		      file mkdir $log_directory
+		  }] \
+		 || ! [file writable $log_directory] \
+		 || [file exists $log_file] } {
+		set log_file $default_error_log_file
+	    }
+	}
+    } else {
+    	set log_file $default_error_log_file
+    }
+
+    puts "Logging to $log_file"
+    set log_ch [open $log_file a]
+    puts $log_ch "----------------------------------"
+    puts $log_ch "$argv0 $argv"
+    puts $log_ch [clock format [clock seconds]]
+    puts $log_ch "pwd: [pwd]"
+    puts $log_ch ""
+    puts $log_ch $error_output
+    puts $log_ch ""
+    puts $log_ch "errorCode: $errorCode"
+    puts $log_ch "errorInfo:"
+    puts $log_ch $errorInfo
+    close $log_ch
+}
+    
 # Construct a mail reply file and send it to the
 # sender of any received mail file.  The option `-all'
 # causes the entire received mail file to be included
@@ -70,17 +132,16 @@ proc reply { args } {
 
     set From_line [file tail [pwd]]
     if { ! [regexp $From_line_regexp $From_line] } {
-        fatal_error \
+        error \
 	    "Current directory name is not\
-	     a mail file `From line':" \
-	     $From_line
+	     a mail file `From line':\n\
+	     \    $From_line"
     }
 
     set to [lindex $From_line 1]
 
     if { ! [file readable $received_file] } {
-        fatal_error \
-	    "Cannot read $received_file"
+        error "Cannot read $received_file"
     }
     set received_ch [open $received_file r]
 
@@ -96,8 +157,7 @@ proc reply { args } {
 
     if { [file exists $history_file] \
          && ! [file writable $history_file] } {
-	 fatal_error \
-	     "Cannot write $history_file"
+	 error "Cannot write $history_file"
     }
 
     if { [file exists $reply_file] } {
@@ -175,23 +235,21 @@ foreach d ". .. ../.. ../../.. ../../../.." {
 # If directory containing judging parameters file
 # is unique, source the file, and set juding_parameters_
 # directory to the directory name.  Otherwise call
-# fatal_error.
+# error.
 #
 if { [llength $judging_parameters_file_list] == 1 } {
     if { [file readable \
                $judging_parameters_file_list] } {
 	source $judging_parameters_file_list
     } else {
-	fatal_error \
-	    "$judging_parameters_file_list\
-	     not readable"
+	error "$judging_parameters_file_list\
+	       not readable"
     }
 } elseif { [llength $judging_parameters_file_list] \
            == 0 } {
-    fatal_error \
-	"$judging_parameters_file not found"
+    error "$judging_parameters_file not found"
 } else {
-    fatal_error \
-	"Too many $judging_parameters_file files:" \
-	$judging_parameters_file_list
+    error \
+	"Too many $judging_parameters_file files:\n\
+	\    $judging_parameters_file_list"
 }
