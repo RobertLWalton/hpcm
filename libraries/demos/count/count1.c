b80ca6c2640f722ad43f4163fb0fb50ccae992a0
acm-cont <acm-cont>
2000-09-07 09:20:39 +0000

    update

---
+++ libraries/demos/count/count1.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+main()
+{
+    int paragraph = 1;
+
+    while ( 1 )
+    {
+	int characters = 0;
+	int words = 0;
+	int lines = 0;
+
+	char buffer [102];
+
+	int at_end_of_file = 1;
+
+	while ( fgets ( buffer, sizeof ( buffer),
+	                stdin ) )
+	{
+	    char * cp = buffer;
+
+	    at_end_of_file = 0;
+
+	    while ( * cp == ' ' ) ++ cp;
+
+	    if ( * cp == 0 || * cp == '\n' ) break;
+
+	    ++ lines;
+
+	    do
+	    {
+		++ words;
+		while ( * cp != ' ' &&
+		        * cp != '\n' &&
+			* cp != 0 ) ++ cp;
+		while ( * cp == ' ' ) ++ cp;
+	    } while ( * cp != 0 && * cp != '\n' );
+
+	    characters += ( cp - buffer );
+	}
+
+	if ( at_end_of_file ) break;
+
+	if ( lines > 0  )
+	{
+	    printf ( "Paragraph %d: %d lines, %d words,"
+	             " %d characters.\n", paragraph,
+		     lines, words, characters );
+
+	    ++ paragraph;
+	}
+    }
+
+    return 1;   /* This line can be omitted.
+		 * It is a test that make count.out
+		 * works even if count returns an
+		 * error code.
+		 */
+}
