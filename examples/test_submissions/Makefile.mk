bfccda9c54f6673494b2a4e77731546c18cd5e48
hc3 <hc3>
2002-02-05 12:16:00 +0000

    update

---
+++ examples/test_submissions/Makefile.mk
@@ -0,0 +1,49 @@
+# Makefile Defining Tests
+#
+# File:		Makefile.mk
+# Author:	Bob Walton (walton@deas.harvard.edu)
+# Date:		Tue Feb  5 07:15:39 EST 2002
+#
+# The authors have placed this program in the public
+# domain; they make no warranty and accept no liability
+# for this program.
+#
+# RCS Info (may not be true date or author):
+#
+#   $Author: hc3 $
+#   $Date: 2002/02/05 12:16:00 $
+#   $RCSfile: Makefile.mk,v $
+#   $Revision: 1.1 $
+
+test_count_correct:	${TEST_PREREQUISITES} \
+	$S/count_correct.c.send \
+	$S/count_correct.cc.send \
+	$S/count_correct.java.send \
+	$S/count_correct.p.send
+
+test_count_incorrect:	${TEST_PREREQUISITES} \
+	$S/count_crash.cc.send \
+	$S/count_timeout.java.send \
+	$S/count_too_much_output.c.send
+
+test_system_error:	${TEST_PREREQUISITES} \
+	$S/system_error.send \
+
+test_get:	${TEST_PREREQUISITES} \
+	$S/get_count.send \
+	$S/get_bad_body.send \
+	$S/get_illegal_filenames.send \
+	$S/get_unreadable_files.send \
+	$S/get_forbidden_files.send
+
+test_query:	${TEST_PREREQUISITES} \
+	$S/query.send \
+	$S/bad_subject_format.send
+
+test_problem_name:	${TEST_PREREQUISITES} \
+	$S/bad_problem_name1.send \
+	$S/bad_problem_name2.send \
+	$S/bad_problem_name3.send \
+	$S/bad_problem_name4.send \
+	$S/bad_problem_name5.send \
+	$S/bad_problem_name6.send
