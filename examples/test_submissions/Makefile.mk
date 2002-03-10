955d76708aa46b14bb23b1b32c7e373892ec32f0
hc3 <hc3>
2002-03-10 01:57:02 +0000

    update

--- examples/test_submissions/Makefile.mk
+++ examples/test_submissions/Makefile.mk
@@ -2,7 +2,7 @@
 #
 # File:		Makefile.mk
 # Author:	Bob Walton (walton@deas.harvard.edu)
-# Date:		Sat Mar  9 19:33:31 EST 2002
+# Date:		Sat Mar  9 20:55:09 EST 2002
 #
 # The authors have placed this program in the public
 # domain; they make no warranty and accept no liability
@@ -11,9 +11,9 @@
 # RCS Info (may not be true date or author):
 #
 #   $Author: hc3 $
-#   $Date: 2002/03/10 00:31:51 $
+#   $Date: 2002/03/10 01:57:02 $
 #   $RCSfile: Makefile.mk,v $
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 
 test_email:	test_count_correct \
 		test_count_incorrect \
@@ -32,7 +32,6 @@ test_informal:	test_count_correct \
 test_formal:	test_count_correct \
 		test_count_incorrect \
 		test_system_error \
-		test_get \
 		test_query \
 		test_problem_name
 
