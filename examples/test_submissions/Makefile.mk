e2901aa612edf1972f04cb5f36a7b479adfc4927
walton <walton>
2007-01-23 02:18:26 +0000

    update

--- examples/test_submissions/Makefile.mk
+++ examples/test_submissions/Makefile.mk
@@ -2,7 +2,7 @@
 #
 # File:		Makefile.mk
 # Author:	Bob Walton (walton@deas.harvard.edu)
-# Date:		Sun Jan 21 05:45:45 EST 2007
+# Date:		Mon Jan 22 07:12:59 EST 2007
 #
 # The authors have placed this program in the public
 # domain; they make no warranty and accept no liability
@@ -11,9 +11,9 @@
 # RCS Info (may not be true date or author):
 #
 #   $Author: walton $
-#   $Date: 2007/01/22 12:04:42 $
+#   $Date: 2007/01/23 02:18:26 $
 #   $RCSfile: Makefile.mk,v $
-#   $Revision: 1.36 $
+#   $Revision: 1.37 $
 
 extract_replies:	mbox
 	rm -rf replies
@@ -55,88 +55,147 @@ diff_replies:
 	    'a response from .*@'; \
 	done
 
-test_basic:	test_count_correct \
-		test_javaio_correct \
-		test_count_incorrect \
-		test_system_error \
-		test_query \
-		test_problem_name
+BASIC_TESTS = \
+	test_count_correct.test \
+	test_javaio_correct.test \
+	test_count_incorrect.test \
+	test_system_error.test \
+	test_query.test \
+	test_problem_name.test
 
-test_email:	test_basic \
-		test_unformatted \
-		test_get
+test_email:	${BASIC_TESTS:.test=.email} \
+		test_unformatted.email \
+		test_get.email
 
-test_local:	test_basic \
-		test_formatted
+test_local:	${BASIC_TESTS:.test=.local} \
+		test_formatted.local
 
-test_informal:	test_basic \
-		test_formatted \
-		test_get
-	@echo "NOTE: If contestants are NOT permitted" \
-	      " to get all problems at once,"
-	@echo "   also execute \`make test_forbidden'"
+test_informal:	${BASIC_TESTS:.test=.local} \
+		test_formatted.local \
+		test_get.local
+
+test_forbidden:	$S/get_forbidden_files.local
 
 test_formal:	test_local
 
-test_untimed:	test_local
+%.email:	%.mail ./Contest_Address ./mail
+	( echo To: `cat Contest_Address`; cat $*.mail) \
+	    | ${SENDMAIL}
+	sleep 2
 
-test_count_correct:	${TEST_PREREQUISITES} \
-	$S/count_correct.c.send \
-	$S/count_correct.cc.send \
-	$S/count_correct.java.send \
-	$S/count_correct.lsp.send
+%.local:	%.mail ./mail
+	hpcm_sendmail <$*.mail
+	sleep 2
 
-test_javaio_correct:	${TEST_PREREQUISITES} \
-	$S/javaio_correct.send
+COUNT_CORRECT_FILES = \
+	$S/count_correct.c.mail \
+	$S/count_correct.cc.mail \
+	$S/count_correct.java.mail \
+	$S/count_correct.lsp.mail
 
-test_count_incorrect:	${TEST_PREREQUISITES} \
-	$S/count_incorrect_output.c.send \
-	$S/count_incomplete_output.cc.send \
-	$S/count_formatting_error.java.send \
-	$S/count_crash.cc.send \
-	$S/count_timeout.java.send \
-	$S/count_timeout.c.send \
-	$S/count_too_much_output.c.send
+JAVAIO_CORRECT_FILES = \
+	$S/javaio_correct.mail
 
-test_formatted:	${TEST_PREREQUISITES} \
-	$S/multipart_formatted.send
+COUNT_INCORRECT_FILES = \
+	$S/count_incorrect_output.c.mail \
+	$S/count_incomplete_output.cc.mail \
+	$S/count_formatting_error.java.mail \
+	$S/count_crash.cc.mail \
+	$S/count_timeout.java.mail \
+	$S/count_timeout.c.mail \
+	$S/count_too_much_output.c.mail
 
-test_unformatted:	${TEST_PREREQUISITES} \
-	$S/unformatted.send \
-	$S/yahoo.send \
-	$S/quoted_printable.send \
-	$S/base64.send \
-	$S/text_plain.send \
-	$S/bad_multipart_encoding.send \
-	$S/bad_multipart_type.send \
-	$S/multipart_7bit.send \
-	$S/multipart_base64.send \
-	$S/multipart_pine.send \
-	$S/multipart_quoted_printable.send \
-	$S/multipart_simple_boundary.send
+FORMATTED_FILES = \
+	$S/multipart_formatted.mail
 
-test_system_error:	${TEST_PREREQUISITES} \
-	$S/system_error.send
+UNFORMATTED_FILES = \
+	$S/unformatted.mail \
+	$S/yahoo.mail \
+	$S/quoted_printable.mail \
+	$S/base64.mail \
+	$S/text_plain.mail \
+	$S/bad_multipart_encoding.mail \
+	$S/bad_multipart_type.mail \
+	$S/multipart_7bit.mail \
+	$S/multipart_base64.mail \
+	$S/multipart_pine.mail \
+	$S/multipart_quoted_printable.mail \
+	$S/multipart_simple_boundary.mail
 
-test_get:	${TEST_PREREQUISITES} \
-	$S/get_count.send \
-	$S/get_bad_body.send \
-	$S/get_illegal_filenames.send \
-	$S/get_unreadable_files.send
+SYSTEM_ERROR_FILES = \
+	$S/system_error.mail
 
-test_forbidden:	$S/get_forbidden_files.send
+GET_FILES = \
+	$S/get_count.mail \
+	$S/get_bad_body.mail \
+	$S/get_illegal_filenames.mail \
+	$S/get_unreadable_files.mail
 
-test_query:	${TEST_PREREQUISITES} \
-	$S/query.send \
-	$S/bad_subject_format.send
+QUERY_FILES = \
+	$S/query.mail \
+	$S/bad_subject_format.mail
 
-test_problem_name:	${TEST_PREREQUISITES} \
-	$S/bad_problem_name1.send \
-	$S/bad_problem_name2.send \
-	$S/bad_problem_name3.send \
-	$S/bad_problem_name4.send \
-	$S/bad_problem_name5.send \
-	$S/bad_problem_name6.send \
-	$S/bad_problem_name7.send \
-	$S/bad_problem_name8.send \
-	$S/bad_problem_name9.send
+PROBLEM_NAME_FILES = \
+	$S/bad_problem_name1.mail \
+	$S/bad_problem_name2.mail \
+	$S/bad_problem_name3.mail \
+	$S/bad_problem_name4.mail \
+	$S/bad_problem_name5.mail \
+	$S/bad_problem_name6.mail \
+	$S/bad_problem_name7.mail \
+	$S/bad_problem_name8.mail \
+	$S/bad_problem_name9.mail
+
+test_count_correct.local:	\
+	${COUNT_CORRECT_FILES:.mail=.local}
+
+test_count_correct.email:	\
+	${COUNT_CORRECT_FILES:.mail=.email}
+
+test_javaio_correct.local:	\
+	${JAVAIO_CORRECT_FILES:.mail=.local}
+
+test_javaio_correct.email:	\
+	${JAVAIO_CORRECT_FILES:.mail=.email}
+
+test_count_incorrect.local:	\
+	${COUNT_INCORRECT_FILES:.mail=.local}
+
+test_count_incorrect.email:	\
+	${COUNT_INCORRECT_FILES:.mail=.email}
+
+test_formatted.local:	\
+	${FORMATTED_FILES:.mail=.local}
+
+test_formatted.email:	\
+	${FORMATTED_FILES:.mail=.email}
+
+test_unformatted.local:	\
+	${UNFORMATTED_FILES:.mail=.local}
+
+test_unformatted.email:	\
+	${UNFORMATTED_FILES:.mail=.email}
+
+test_system_error.local:	\
+	${SYSTEM_ERROR_FILES:.mail=.local}
+
+test_system_error.email:	\
+	${SYSTEM_ERROR_FILES:.mail=.email}
+
+test_get.local:	\
+	${GET_FILES:.mail=.local}
+
+test_get.email:	\
+	${GET_FILES:.mail=.email}
+
+test_query.local:	\
+	${QUERY_FILES:.mail=.local}
+
+test_query.email:	\
+	${QUERY_FILES:.mail=.email}
+
+test_problem_name.local:	\
+	${PROBLEM_NAME_FILES:.mail=.local}
+
+test_problem_name.email:	\
+	${PROBLEM_NAME_FILES:.mail=.email}
