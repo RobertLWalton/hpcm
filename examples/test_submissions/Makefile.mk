8b269dd80cc95640f40b33f5c6e56a9dcd384570
walton <walton>
2007-01-24 08:04:48 +0000

    update

--- examples/test_submissions/Makefile.mk
+++ examples/test_submissions/Makefile.mk
@@ -2,7 +2,7 @@
 #
 # File:		Makefile.mk
 # Author:	Bob Walton (walton@deas.harvard.edu)
-# Date:		Wed Jan 24 01:48:41 EST 2007
+# Date:		Wed Jan 24 03:03:33 EST 2007
 #
 # The authors have placed this program in the public
 # domain; they make no warranty and accept no liability
@@ -11,9 +11,9 @@
 # RCS Info (may not be true date or author):
 #
 #   $Author: walton $
-#   $Date: 2007/01/24 06:58:58 $
+#   $Date: 2007/01/24 08:04:48 $
 #   $RCSfile: Makefile.mk,v $
-#   $Revision: 1.42 $
+#   $Revision: 1.43 $
 
 COMMON_TESTS = \
 	test_count_correct \
@@ -179,28 +179,28 @@ PROBLEM_NAME_FILES = \
 	bad_problem_name9
 
 test_count_correct.local:	\
-	${COUNT_CORRECT_FILES:%=$S/%.local}
+    ${COUNT_CORRECT_FILES:%=$S/%.local}
 
 test_count_correct.email:	\
-	${COUNT_CORRECT_FILES:%=$S/%.email}
+    ${COUNT_CORRECT_FILES:%=$S/%.email}
 
 test_count_correct.diff:	\
-	${COUNT_CORRECT_FILES:%=$S/replies/%.diff}
+    ${COUNT_CORRECT_FILES:%=$S/common_replies/%.diff}
 
 test_javaio_correct.local:	\
-	${JAVAIO_CORRECT_FILES:%=$S/%.local}
+    ${JAVAIO_CORRECT_FILES:%=$S/%.local}
 
 test_javaio_correct.email:	\
-	${JAVAIO_CORRECT_FILES:%=$S/%.email}
+    ${JAVAIO_CORRECT_FILES:%=$S/%.email}
 
 test_javaio_correct.diff:	\
-	${JAVAIO_CORRECT_FILES:%=$S/replies/%.diff}
+    ${JAVAIO_CORRECT_FILES:%=$S/common_replies/%.diff}
 
 test_count_incorrect.local:	\
-	${COUNT_INCORRECT_FILES:%=$S/%.local}
+    ${COUNT_INCORRECT_FILES:%=$S/%.local}
 
 test_count_incorrect.email:	\
-	${COUNT_INCORRECT_FILES:%=$S/%.email}
+    ${COUNT_INCORRECT_FILES:%=$S/%.email}
 
 test_count_incorrect.auto_diff:	\
     ${COUNT_INCORRECT_FILES:%=$S/auto_replies/%.diff}
@@ -209,70 +209,70 @@ test_count_incorrect.manual_diff:	\
     ${COUNT_INCORRECT_FILES:%=$S/manual_replies/%.diff}
 
 test_formatted.local:	\
-	${FORMATTED_FILES:%=$S/%.local}
+    ${FORMATTED_FILES:%=$S/%.local}
 
 test_formatted.email:	\
-	${FORMATTED_FILES:%=$S/%.email}
+    ${FORMATTED_FILES:%=$S/%.email}
 
 test_formatted.diff:	\
-	${FORMATTED_FILES:%=$S/replies/%.diff}
+    ${FORMATTED_FILES:%=$S/common_replies/%.diff}
 
 test_unformatted.local:	\
-	${UNFORMATTED_FILES:%=$S/%.local}
+    ${UNFORMATTED_FILES:%=$S/%.local}
 
 test_unformatted.email:	\
-	${UNFORMATTED_FILES:%=$S/%.email}
+    ${UNFORMATTED_FILES:%=$S/%.email}
 
 test_unformatted.diff:	\
-	${UNFORMATTED_FILES:%=$S/replies/%.diff}
+    ${UNFORMATTED_FILES:%=$S/common_replies/%.diff}
 
 test_system_error.local:	\
-	${SYSTEM_ERROR_FILES:%=$S/%.local}
+    ${SYSTEM_ERROR_FILES:%=$S/%.local}
 
 test_system_error.email:	\
-	${SYSTEM_ERROR_FILES:%=$S/%.email}
+    ${SYSTEM_ERROR_FILES:%=$S/%.email}
 
 test_system_error.auto_diff:	\
-	${SYSTEM_ERROR_FILES:%=$S/auto_replies/%.diff}
+    ${SYSTEM_ERROR_FILES:%=$S/auto_replies/%.diff}
 
 test_system_error.manual_diff:	\
-	${SYSTEM_ERROR_FILES:%=$S/manual_replies/%.diff}
+    ${SYSTEM_ERROR_FILES:%=$S/manual_replies/%.diff}
 
 test_get.local:	\
-	${GET_FILES:%=$S/%.local}
+    ${GET_FILES:%=$S/%.local}
 
 test_get.email:	\
-	${GET_FILES:%=$S/%.email}
+    ${GET_FILES:%=$S/%.email}
 
 test_get.diff:	\
-	${GET_FILES:%=$S/replies/%.diff}
+    ${GET_FILES:%=$S/common_replies/%.diff}
 
 test_forbidden.local:	\
-	${FORBIDDEN_FILES:%=$S/%.local}
+    ${FORBIDDEN_FILES:%=$S/%.local}
 
 test_forbidden.email:	\
-	${FORBIDDEN_FILES:%=$S/%.email}
+    ${FORBIDDEN_FILES:%=$S/%.email}
 
 test_forbidden.diff:	\
-	${FORBIDDEN_FILES:%=$S/replies/%.diff}
+    ${FORBIDDEN_FILES:%=$S/common_replies/%.diff}
 
 test_query.local:	\
-	${QUERY_FILES:%=$S/%.local}
+    ${QUERY_FILES:%=$S/%.local}
 
 test_query.email:	\
-	${QUERY_FILES:%=$S/%.email}
+    ${QUERY_FILES:%=$S/%.email}
 
 test_query.auto_diff:	\
-	${QUERY_FILES:%=$S/auto_replies/%.diff}
+    ${QUERY_FILES:%=$S/auto_replies/%.diff}
 
 test_query.manual_diff:	\
-	${QUERY_FILES:%=$S/manual_replies/%.diff}
+    ${QUERY_FILES:%=$S/manual_replies/%.diff}
 
 test_problem_name.local:	\
-	${PROBLEM_NAME_FILES:%=$S/%.local}
+    ${PROBLEM_NAME_FILES:%=$S/%.local}
 
 test_problem_name.email:	\
-	${PROBLEM_NAME_FILES:%=$S/%.email}
+    ${PROBLEM_NAME_FILES:%=$S/%.email}
 
 test_problem_name.diff:	\
-	${PROBLEM_NAME_FILES:%=$S/replies/%.diff}
+    ${PROBLEM_NAME_FILES:%=$S/common_replies/%.diff}
