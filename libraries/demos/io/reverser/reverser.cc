67198a444196dc7a8743bd9b3c0526acc9fd5193
walton <walton>
2013-01-17 07:30:39 +0000

    update

--- libraries/demos/io/reverser/reverser.cc
+++ libraries/demos/io/reverser/reverser.cc
@@ -2,7 +2,7 @@
 //
 // File:	reverser.cc
 // Authors:	Bob Walton (walton@seas.harvard.edu)
-// Date:	Wed Jan 16 20:18:36 EST 2013
+// Date:	Thu Jan 17 02:28:50 EST 2013
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
@@ -11,29 +11,30 @@
 // RCS Info (may not be true date or author):
 //
 //   $Author: walton $
-//   $Date: 2013/01/17 01:38:04 $
+//   $Date: 2013/01/17 07:30:39 $
 //   $RCSfile: reverser.cc,v $
-//   $Revision: 1.1 $
+//   $Revision: 1.2 $
 
 #include <iostream>
+using namespace std;
 #include <cctype>
 #include <cassert>
-using namespace std;
 
-int debug;
-#define dout if ( debug ) cout
+int debug = false;
+#define dout    if ( debug ) cout
+#define dprintf if ( debug ) printf
 
-char line[82];
+char line[81];
 
-/* Function to find and print the first word BEFORE q,
- * where q points into `line'.  It is a program error
- * if there is no such word.  Returns pointer to the
- * first character of the word printed.
- */
+// Function to find and print the first word BEFORE q,
+// where q points into `line'.  It is a program error
+// if there is no such word.  Returns pointer to the
+// first character of the word printed.
+//
 char * print_substitute_word ( char * q )
 {
-    /* Move q backward to point just after word.
-     */
+    // Move q backward to point just after word.
+    //
     while ( 1 )
     {
         assert ( q > line );
@@ -42,13 +43,13 @@ char * print_substitute_word ( char * q )
     }
     char * p = q;  /* p points just after word. */
 
-    /* Move q to point at 1st character of word.
-     */
+    // Move q to point at 1st character of word.
+    //
     -- q;
     while ( q > line && isalpha ( q[-1] ) ) -- q;
 
-    /* Print word and return.
-     */
+    // Print word and return.
+    //
     char save = * p;
     * p = 0;
     cout << q;
@@ -63,35 +64,35 @@ int main ( int argc, char * argv[] )
     while ( cin.getline ( line, sizeof ( line ) ),
             ! cin.eof() )
     {
-	/* When debugging print the intput as well as
-	 * the output.
-	 */
+	// When debugging print the intput as well as
+	// the output.
+	//
 	dout << line << endl;
 
-    	/* Set p to beginning of line and q to end of
-	 * line.
-	 */
+    	// Set p to beginning of line and q to end of
+	// line.
+	//
 	char * p = line, * q = line;
 	while ( * q ) ++ q;
 
-	/* Print line substituting for words.
-	 */
+	// Print line substituting for words.
+	//
 	while ( * p )
 	{
 	    if ( isalpha ( * p ) )
 	    {
-		/* At start of word. Skip word.
-		 */
+		// At start of word. Skip word.
+		//
 	        while ( isalpha ( * p ) ) ++ p;
 
-		/* Substitute for skipped word.
-		 */
+		// Substitute for skipped word.
+		//
 		q = print_substitute_word ( q );
 	    }
 	    else
 	    {
-	        /* Print and skip non-letter.
-		 */
+	        // Print and skip non-letter.
+		//
 	        cout << * p ++;
 	    }
 	}
