57735ac870dadbd88bbf5e0bfdafbd6980cfff67
walton <walton>
2013-05-06 08:59:21 +0000

    update

--- test_submissions/Makefile.mk
+++ test_submissions/Makefile.mk
@@ -2,7 +2,7 @@
 #
 # File:		Makefile.mk
 # Author:	Bob Walton (walton@seas.harvard.edu)
-# Date:		Thu May  2 07:30:09 EDT 2013
+# Date:		Sun May  5 16:18:05 EDT 2013
 #
 # The authors have placed this program in the public
 # domain; they make no warranty and accept no liability
@@ -11,51 +11,38 @@
 # RCS Info (may not be true date or author):
 #
 #   $Author: walton $
-#   $Date: 2013/05/02 14:51:58 $
+#   $Date: 2013/05/06 08:59:21 $
 #   $RCSfile: Makefile.mk,v $
-#   $Revision: 1.1 $
+#   $Revision: 1.2 $
 
 CORRECT_TESTS = \
-    reverser_correct_cc \
-    reverser_correct_c \
-    reverser_correct_java \
-    reverser_correct_lsp
+    $S/reverser_correct_cc.mail \
+    $S/reverser_correct_c.mail \
+    $S/reverser_correct_java.mail \
+    $S/reverser_correct_lsp.mail
 
 INCORRECT_TESTS = \
-    reverser_formatting_error_cc \
-    reverser_incomplete_output_cc \
-    reverser_incorrect_output_cc \
-    reverser_no_output_crash_cc \
-    reverser_segment_fault_crash_cc \
-    reverser_too_much_cpu_time_cc \
-    reverser_too_much_output_cc
+    $S/reverser_formatting_error_cc.mail \
+    $S/reverser_incomplete_output_cc.mail \
+    $S/reverser_incorrect_output_cc.mail \
+    $S/reverser_no_output_crash_cc.mail \
+    $S/reverser_segment_fault_crash_cc.mail \
+    $S/reverser_too_much_cpu_time_cc.mail \
+    $S/reverser_too_much_output_cc.mail
 
 EMAIL_TESTS = \
-    bad_multipart_encoding \
-    bad_multipart_type \
-    multipart_reverser_correct_java
+    $S/bad_multipart_encoding.mail \
+    $S/bad_multipart_type.mail \
+    $S/multipart_reverser_correct_java.mail
 
 EXTRACTION_TESTS = \
-    bad_problem_name1 \
-    bad_problem_name2 \
-    bad_problem_name3 \
-    bad_problem_name4 \
-    bad_problem_name5 \
-    bad_problem_name6 \
-    bad_problem_name7 \
-    bad_problem_name8 \
-    bad_problem_name9 \
-    bad_subject_format
-
-%.send:		$S/%.mail
-	testsubmit $S/$*.mail
-
-test_basics:	test_correct test_incorrect
-
-test_correct:	${CORRECT_TESTS:%=%.send}
-
-test_incorrect:	${INCORRECT_TESTS:%=%.send}
-
-test_extraction:	${EXTRACTION_TESTS:%=%.send}
-
-test_email:	${EMAIL_TESTS:%=%.send}
+    $S/bad_problem_name1.mail \
+    $S/bad_problem_name2.mail \
+    $S/bad_problem_name3.mail \
+    $S/bad_problem_name4.mail \
+    $S/bad_problem_name5.mail \
+    $S/bad_problem_name6.mail \
+    $S/bad_problem_name7.mail \
+    $S/bad_problem_name8.mail \
+    $S/bad_problem_name9.mail \
+    $S/bad_subject_format.mail
