8791ff0b05ede087763ca5a5604b6c4df2aa9004
Robert L Walton <walton@acm.org>
2018-09-19 02:54:29 -0400

    update

--- contestant/src/hpcm_general_position.cc
+++ contestant/src/hpcm_general_position.cc
@@ -2,7 +2,7 @@
 //
 // File:	hpcm_general_position.cc
 // Authors:	Bob Walton (walton@deas.harvard.edu)
-// Date:	Tue Sep 18 07:43:56 EDT 2018
+// Date:	Wed Sep 19 02:36:27 EDT 2018
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
@@ -12,15 +12,18 @@
 #include <cstdlib>
 #include <cstdio>
 #include <cstring>
+#include <cmath>
 #include <cassert>
 using std::cout;
 using std::cerr;
 using std::endl;
 using std::cin;
+using std::ostream;
 
 extern "C" {
 # include <unistd.h>
 # include <fcntl.h>
+# include <time.h>
 }
 
 bool debug = false;
@@ -51,8 +54,9 @@ bool binary = false;
 
 long N, xmin, xmax, ymin, ymax;
 
-long n;
+long n;  // There are n points so far.
 struct point { long x, y; } * points;
+long m;  // There are m lines so far.
 struct line { long a, b; long long c; } * lines;
     // So far there are n <= N points and (n-1)*n lines.
     // A new point must not be on any previous line,
@@ -176,6 +180,101 @@ int main ( int argc, char ** argv )
     close ( rfd );
     srandom ( seed );
 
+    points = new point[N];
+    lines = new line[(N-1)*N];
+
+    n = m = 0;
+    cerr << endl;
+    ostream & out = cout;
+    timespec time1;
+    clock_gettime ( CLOCK_MONOTONIC_COARSE, & time1 );
+    double failed_attempts = 0;
+    long long attempts = 0;
+    while ( n < N )
+    {
+    	++ attempts;
+        long x = xmin + random() % ( xmax - xmin + 1 );
+        long y = ymin + random() % ( ymax - ymin + 1 );
+
+	bool OK = true;
+
+	// Check that (x,y) is not on any line.  Note
+	// that if (x,y) equals a previous point, it
+	// will be on lines containing that point.
+	//
+	for ( int i = 0; OK && i < m; ++ i )
+	{
+	    line & l = lines[i];
+	    if (    (long long) l.a * x
+	          + (long long) l.b * y
+		 == l.c )
+	        OK = false;
+	}
+	if ( ! OK ) continue;
+
+	// For each previous point create a new line
+	// passing through it an the new point.
+	//
+	for ( int i = 0; i < n; ++ i )
+	{
+	    point & p = points[i];
+	    line & l = lines[m++];
+	    long dx = p.x - x;
+	    long dy = p.y - y;
+	    l.a = - dy;
+	    l.b = dx;
+	    l.c = - (long long) dy * x
+	          + (long long) dx * y;
+	}
+
+	point & p = points[n++];
+	p.x = x;
+	p.y = y;
+
+	char buffer[100];
+	sprintf ( buffer, "{ %12d, %12d },", x, y );
+	out << buffer << endl;
+
+	-- attempts;
+	failed_attempts *= 0.99;
+	failed_attempts += 0.01 * attempts;
+	attempts = 0;
+
+	if ( n >= 300 )
+	{
+	    timespec time2;
+	    clock_gettime
+	        ( CLOCK_MONOTONIC_COARSE, & time2 );
+	    double dtime =
+	          time2.tv_sec - time1.tv_sec
+		+ 1e-9
+		* ( time2.tv_nsec - time1.tv_nsec );
+
+	    double etime = (double) N / n;
+	    etime = etime * etime * etime * dtime;
+	    etime -= dtime;
+
+	    char tbuffer[100];
+	    sprintf ( tbuffer,
+	              "%6d points"
+		      "%3.0f:%02.0f:%02.0f"
+		      " to finish"
+		      "%3.0f:%02.0f:%02.0f"
+		      " time"
+		      "%5.0f failed attempts per point",
+		      n,
+		      etime / 3600,
+		      fmod ( etime / 60, 60 ),
+		      fmod ( etime, 60 ),
+		      dtime / 3600,
+		      fmod ( dtime / 60, 60 ),
+		      fmod ( dtime, 60 ),
+		      failed_attempts );
+
+	    cerr << "\r" << tbuffer;
+	}
+    }
+    cerr << endl;
 
     return 0;
 }
